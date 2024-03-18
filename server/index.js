15771ca97714f1d6cbec0384c45eafe76529de6d
amujkic1 <amujkic1@etf.unsa.ba>
2024-03-18 11:09:20 +0100

    added directories

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()   
+// Create an Express application
+const app = express();
 
-const PORT = 3000
+// Define a route handler for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+// Start the server and listen on port 3000
+const port = 3002;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
