098bd1310412c638daeb5496c60f50810481801a
amujkic1 <amujkic1@etf.unsa.ba>
2024-03-23 21:47:50 +0100

    updates in database.js

--- server/database.js
+++ server/database.js
@@ -1,25 +1,24 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+    connectionString: process.env.POSTGRES_URL,
 });
 
 // Simple query to check if the database is working
 const checkDatabase = async () => {
   try {
-    console.log("ENV: ", process.env.POSTGRES_URL);
     const client = await pool.connect();
+    const result = await client.query('SELECT NOW() AS current_time');
+    console.log('Database is working fine. Current time in the database:', result.rows[0].current_time);
+    client.release(); 
   } catch (error) {
-    console.error("Error connecting to the database:", error);
-  } finally {
-    // Close the connection pool
-    await pool.end();
+    console.error('Error connecting to the database:', error);
   }
 };
 
-checkDatabase()
 
 module.exports = {
-  pool
+  pool,
+  checkDatabase, // Exporting checkDatabase function
 };
