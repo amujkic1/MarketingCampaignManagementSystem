2b8a886d16d6030815274592902da99abdef264f
Ilhana <iosmankovi2@etf.unsa.ba>
2024-03-23 14:59:14 +0100

    Modified App.jsx and AuthStatus

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '/HomePage.jsx';
+import '/AuthStatus.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Application status
 
+  // // Function to login
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  // Logout feature
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
   return (
     <>
       <div>
@@ -24,7 +36,21 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+           {/* Show login button if user is not logged in */}
+           {!isLoggedIn && (
+          <button onClick={login}>
+            Login
+          </button>
+        )}
+        {/* Show logout button if user is logged in */}
+        {isLoggedIn && (
+          <button onClick={logout}>
+            Logout
+          </button>
+        )}
       </div>
+      {/* Show HomePage only if user is logged in */}
+      {isLoggedIn && <HomePage />}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
