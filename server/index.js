b3b46b4483353e4e6915b5ccdaf9d82db5cfefcf
amujkic1 <amujkic1@etf.unsa.ba>
2024-03-24 22:33:11 +0100

    generated swagger documentation

--- server/index.js
+++ server/index.js
@@ -5,14 +5,18 @@ const authRouter = require("./routes/authRouter");
 const bodyParser = require("body-parser");
 const { Pool } = require("pg");
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
-const port = 3002;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use("/user", authRouter);
 app.use("/qrimage", authRouter);
 app.use("/set2FA", authRouter);
 app.use(cors());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
