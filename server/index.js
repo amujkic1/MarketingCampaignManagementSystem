b387017d42867477893661df5e6f9eebb88a9f07
amujkic1 <amujkic1@etf.unsa.ba>
2024-03-25 22:17:45 +0100

    code cleanup

--- server/index.js
+++ server/index.js
@@ -3,28 +3,9 @@ const cors = require('cors');
 const { pool } = require('./database');
 
 const app = express();
-const port = 3002;
+const port = 3000;
 
-// Allow requests from all origins (you can customize this based on your requirements)
 app.use(cors());
-
-app.get('/hello', (req, res) => {
-  res.send('Hello, world!');
-});
-
-app.get('/users', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM users');
-      const users = result.rows;
-      res.json(users);
-      client.release();
-    } catch (error) {
-      console.error('Error fetching users:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
