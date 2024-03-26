3e34bb471f4078cbbf947318bee3b73494affd43
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2024-03-26 15:01:37 +0100

    Chnages login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Login.css'
 
 function Login() {
+  const [emailOrPhone, setEmailOrPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleLogin() {
+    fetch('https://localhost:3002/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ emailOrPhone, password })
+    })
+      .then(response => {
+        if (response.ok) {
+          // Ovdje možete obraditi odgovor ako je prijava uspješna
+          console.log('Login successful');
+          // Resetovanje polja za unos
+          setEmailOrPhone('');
+          setPassword('');
+          setErrorMessage('');
+        } else {
+          // Ovdje možete obraditi grešku ako prijava nije uspjela
+          return response.json().then(data => {
+            throw new Error(data.message);
+          });
+        }
+      })
+      .catch(error => {
+        // Ovdje se hvataju sve vrste grešaka
+        console.error('Login error:', error);
+        setErrorMessage('Failed to login. Please try again.');
+      });
+  }
 
   return (
     <div className="container-fluid d-flex justify-content-center align-items-center vh-100">
@@ -10,18 +44,21 @@ function Login() {
             <div className='card-body p-5 d-flex flex-column align-items-center'>
               <h2 className="fw-bold mb-2 text-uppercase">Log in</h2>
               <p className="text-muted text-center mb-5">Please enter your email and password!</p>
+
               <div className='mb-4 mx-5 w-100'>
                 <label className='form-label' htmlFor='email'>Email or phone number</label>
-                <input className='form-control' id='email' type='email' />
+                <input className='form-control' id='email' type='email' value={emailOrPhone} onChange={e => setEmailOrPhone(e.target.value)} />
               </div>
               <div className='mb-4 mx-5 w-100'>
                 <label className='form-label' htmlFor='password'>Password</label>
-                <input className='form-control' id='password' type='password' />
+                <input className='form-control' id='password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
               </div>
+
               <p className="small mb-3 pb-lg-2"><a className="text-muted" href="#!">Forgot password?</a></p>
-              <button className='btn btn-primary mx-2 px-5' type='button'>
+              <button className='btn btn-primary mx-2 px-5' type='button' onClick={handleLogin}>
                 Log in
               </button>
+              {errorMessage && <p className="text-danger">{errorMessage}</p>}
               <div className='d-flex flex-row mt-3 mb-5'></div>
               <div>
                 <p className="mb-0">Don't have an account? <a href="#!" className="text-muted fw-bold">Sign Up</a></p>
