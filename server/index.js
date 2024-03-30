b16d18f4ac5a49528f365427b773d9cfe33e43ee
amujkic1 <amujkic1@etf.unsa.ba>
2024-03-30 15:05:46 +0100

    implemented routes for super admin

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const { pool } = require('./database');
 const authRouter = require("./routes/authRouter");
+const superAdminRouter = require("./routes/superAdminRouter.js"); 
 const bodyParser = require("body-parser");
 const { Pool } = require("pg");
 const app = express();
@@ -13,27 +14,28 @@ const authMiddleware = require('./middleware/authMiddleware.js');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
 
-app.use(cors({
+/*app.use(cors({
   origin: 'https://marketing-campaign-management-system-client.vercel.app',
   credentials: true
-}));
+}));*/
 
 app.use("/user", authRouter);
 app.use("/qrimage", authRouter);
 app.use("/set2FA", authRouter);
 app.use("/getUser", authRouter);
 
+//za super admina
+app.use("/super", superAdminRouter);
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json());
-
 
 app.use("/", authRouter);
 
 const port = 3000;
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
