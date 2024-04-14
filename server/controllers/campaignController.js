ff42213834faaa281d23a21f7bb988881d16ffdf
nejrastr <86197301+nejrastr@users.noreply.github.com>
2024-04-14 13:51:53 +0200

    implemented zip and download feature

--- server/controllers/campaignController.js
+++ server/controllers/campaignController.js
@@ -1,5 +1,8 @@
 const { pool } = require("../database");
 const Campaign = require("../models/campaign");
+const archiver = require("archiver");
+const fs = require("fs");
+const axios = require("axios");
 
 async function createCampaign(req, res) {
   const { name, durationfrom, durationto, mediatypes, channels } = req.body;
@@ -47,11 +50,67 @@ async function getCampaignsById(req, res) {
       .json({ message: "Failed to return campaign from database" });
   }
 }
+async function getCampaignMedia(req, res) {
+  const { id } = req.params;
+  console.log(req.params);
+  try {
+    const campaignMedia = await Campaign.getCampaignMedia(pool, id);
+    console.log(campaignMedia);
+
+    const archive = archiver("zip", { zlib: { level: 9 } });
+
+    res.attachment(`campaign_${id}_media.zip`);
+    res.status(200);
+
+    archive.pipe(res);
+
+    for (const media of campaignMedia) {
+      if (media.url) {
+        let response;
+        switch (media.mediatype) {
+          case "Video":
+          case "Audio":
+          case "Image":
+            response = await axios.get(media.url, { responseType: "stream" });
+            archive.append(response.data, { name: media.mediatype });
+            break;
+          case "Text":
+            archive.append(media.url, { name: `${media.mediatype}.txt` });
+            break;
+          case "Link":
+            archive.append(media.url, { name: `${media.mediatype}.link` });
+            break;
+          case "Banner":
+            archive.append(media.banner_link, {
+              name: `${media.mediatype}.html`,
+            });
+            break;
+        }
+      }
+    }
+
+    archive.finalize();
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Failed to return mediatypes for certain campaign" });
+  }
+}
+
 async function updateCampaign(req, res) {
   const { id } = req.params;
   const { name, durationfrom, durationto, mediatypes, channels } = req.body;
   try {
-    const campaign = await Campaign.updateCampaign(pool, id, name, durationfrom, durationto, mediatypes, channels);
+    const campaign = await Campaign.updateCampaign(
+      pool,
+      id,
+      name,
+      durationfrom,
+      durationto,
+      mediatypes,
+      channels
+    );
     res.status(200).json({ message: "Successfuly updated campaign" });
   } catch (error) {
     console.log(error);
@@ -75,4 +134,5 @@ module.exports = {
   getCampaignsById,
   updateCampaign,
   deleteCampaign,
+  getCampaignMedia,
 };
