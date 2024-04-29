eeaec607cce14bb488a15327a17998c235bd9ec1
Faris Crnčalo <115996601+fcrncalo1@users.noreply.github.com>
2024-04-29 17:40:58 +0200

    Added route for getting all campaigns for a channel

--- server/controllers/channelController.js
+++ server/controllers/channelController.js
@@ -52,6 +52,15 @@ async function updateChannel(req, res) {
     res.status(500).json({ message: "Failed to update channel" });
   }
 }
+async function getAllCampaignsForChannel(req, res) {
+  const { id } = req.params
+  try {
+    const allCampaigns = await Channel.getAllCampaignsForChannel(pool, id);
+    res.status(200).json(allCampaigns);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve all campaigns for a channel' });
+  }
+}
 
 module.exports = {
   createChannel,
@@ -59,4 +68,5 @@ module.exports = {
   getChannelById,
   deleteChannel,
   updateChannel,
+  getAllCampaignsForChannel
 };
